2dc27cb6900e34dd8d4a99ddcb1d2ae8fae189cd
Matthew Grainger <DrMattG@users.noreply.github.com>
2022-10-25 12:43:42 +0200

    Add test data for dataviz until upload is finished

--- inst/shiny-examples/CiteSource/app.R
+++ inst/shiny-examples/CiteSource/app.R
@@ -7,6 +7,7 @@ library(shinyWidgets)
 library(htmltools)
 library(markdown)
 library(CiteSource)
+library(plotly)
 options(shiny.maxRequestSize=30*1024^2)
 # Set background colour
 tags$head(tags$style(
@@ -39,7 +40,7 @@ ui <- navbarPage("CiteSource", id = "tabs",
                  ),
                  
                  # File upload tab
-                 tabPanel("File upload",
+                 tabPanel("File upload & Deduplicate",
                           fluidRow(
                             column(12,
                                    # Sidebar layout with input and output definitions ----
@@ -66,35 +67,6 @@ ui <- navbarPage("CiteSource", id = "tabs",
                           )
                  ),
                  
-                 # Deduplication tab
-                 tabPanel("Deduplicate",
-                          
-                          fluidRow(
-                            
-                            column(12,
-                                   actionButton("deduplicate", "Deduplicate citations"),
-                                     
-                                     # Main panel for displaying outputs ----
-                                     mainPanel(
-                                       
-                                       tabsetPanel(
-                                         
-                                         tabPanel("Deduplication results",
-                                                  
-                                                  tableOutput("dedup_results_table")
-                                                  
-                                         # ),
-                                         # tabPanel("Source comparison table",
-                                         #          
-                                         #          dataTableOutput("comparison_table")
-                                                  
-                                         )
-                                         
-                                       ))
-                                     
-                                   ))),
-                 
-                 
                  # Visualise tab
                  tabPanel("Visualise",
                           fluidRow(
@@ -112,9 +84,13 @@ ui <- navbarPage("CiteSource", id = "tabs",
                                                            'Select tags',
                                                            uiOutput('checkbox2')),
                                               
-                                              mainPanel('Your visualisations will appear here')
-                                            )))
-                            ))),
+                                              mainPanel(
+                                                tabsetPanel(
+                                                  tabPanel("Plot overlap as a heatmap matrix", plotly::plotlyOutput("plotgraph1")),
+                                                  tabPanel("Plot overlap as an upset plot", downloadButton("downloadPlot"),
+                                                           plotOutput("plotgraph2"))
+                                                  ))))
+                            )))),
                  
                  
                  # Export tab
@@ -179,264 +155,55 @@ server <- function(input, output, session) {
   
     })
   })
-   
-  # observeEvent(input$source_selection, {
-  #   
-  #   relevant_choices <- field_names() 
-  #   relevant_choices <- relevant_choices[[input$source_selection]]
-  #   
-  #   index <- match('end_page',relevant_choices)
-  #   
-  #   if(is.na(index)){
-  #     
-  #     index <- 1
-  #   }
-  #   
-  #   updateSelectInput(session, "end_page_field_match",
-  #                     label = "End page",
-  #                     choices =  c("None", relevant_choices),
-  #                     selected = relevant_choices[index])
-  #   
-  # })
-  # 
-  # observeEvent(input$source_selection, {
-  #   
-  #   relevant_choices <- field_names() 
-  #   relevant_choices <- relevant_choices[[input$source_selection]]
-  #   
-  #   index <- match('pages',relevant_choices)
-  #   
-  #   if(is.na(index)){
-  #     
-  #     index <- 1
-  #   }
-  #   
-  #   updateSelectInput(session, "pages_field_match",
-  #                     label = "Pages",
-  #                     choices =  c("None", relevant_choices),
-  #                     selected = relevant_choices[index])
-  #   
-  # })
-  # 
-  # observeEvent(input$source_selection, {
-  #   
-  #   relevant_choices <- field_names() 
-  #   relevant_choices <- relevant_choices[[input$source_selection]]
-  #   
-  #   index <- match('volume',relevant_choices)
-  #   
-  #   if(is.na(index)){
-  #     
-  #     index <- 1
-  #   }
-  #   
-  #   updateSelectInput(session, "volume_field_match",
-  #                     label = "Volume",
-  #                     choices =  c("None", relevant_choices),
-  #                     selected = relevant_choices[index])
-  #   
-  # })
-  # 
-  # observeEvent(input$source_selection, {
-  #   
-  #   relevant_choices <- field_names() 
-  #   relevant_choices <- relevant_choices[[input$source_selection]]
-  #   
-  #   index <- match('issue',relevant_choices)
-  #   
-  #   if(is.na(index)){
-  #     
-  #     index <- 1
-  #   }
-  #   
-  #   updateSelectInput(session, "number_field_match",
-  #                     label = "Issue number",
-  #                     choices =  c("None", relevant_choices),
-  #                     selected = relevant_choices[index])
-  #   
-  # })
-  # 
-  # observeEvent(input$source_selection, {
-  #   
-  #   relevant_choices <- field_names() 
-  #   relevant_choices <- relevant_choices[[input$source_selection]]
-  #   
-  #   index <- match('abstract',relevant_choices)
-  #   
-  #   if(is.na(index)){
-  #     
-  #     index <- 1
-  #   }
-  #   
-  #   updateSelectInput(session, "abstract_field_match",
-  #                     label = "Abstract",
-  #                     choices =  c("None", relevant_choices),
-  #                     selected = relevant_choices[index])
-  #   
-  # })
-  # 
-  # observeEvent(input$source_selection, {
-  #   
-  #   relevant_choices <- field_names() 
-  #   relevant_choices <- relevant_choices[[input$source_selection]]
-  #   
-  #   index <- match('isbn',relevant_choices)
-  #   
-  #   if(is.na(index)){
-  #     
-  #     index <- 1
-  #   }
-  #   
-  #   updateSelectInput(session, "isbn_field_match",
-  #                     label = "ISBN",
-  #                     choices =  c("None", relevant_choices),
-  #                     selected = relevant_choices[index])
-  #   
-  # })
-  # 
-  # # display summary input table1
-  # output$db_name <- renderDataTable({
-  #   DT::datatable(rv$df, 
-  #                 options = list(paging = FALSE, 
-  #                                searching = FALSE),
-  #                 rownames = FALSE)
-  # })
-  # 
-  # 
-  # # when deduplication button clicked, deduplicate citations using ASYSD and merge citations together
-  # unique_citations <- eventReactive(input$deduplicate, {
-  #   
-  #   all_citations <- rv$upload_df
-  #   
-  #   names(all_citations)[which(names(all_citations) == input$title_field_match)] <- "title"
-  #   names(all_citations)[which(names(all_citations) == input$author_field_match)] <- "author"
-  #   names(all_citations)[which(names(all_citations) == input$year_field_match)] <- "year"
-  #   names(all_citations)[which(names(all_citations) == input$pages_field_match)] <- "pages"
-  #   names(all_citations)[which(names(all_citations) == input$volume_field_match)] <- "volume"
-  #   names(all_citations)[which(names(all_citations) == input$number_field_match)] <- "number"
-  #   names(all_citations)[which(names(all_citations) == input$journal_field_match)] <- "journal"
-  #   names(all_citations)[which(names(all_citations) == input$doi_field_match)] <- "doi"
-  #   names(all_citations)[which(names(all_citations) == input$start_page_field_match)] <- "start_page"
-  #   names(all_citations)[which(names(all_citations) == input$end_page_field_match)] <- "end_page"
-  #   names(all_citations)[which(names(all_citations) == input$isbn_field_match)] <- "isbn"
-  #   names(all_citations)[which(names(all_citations) == input$abstract)] <- "abstract"
-  #   names(all_citations)[which(names(all_citations) == input$record_id)] <- "record_id"
-  #   names(all_citations)[which(names(all_citations) ==  "cs_source")] <- "source"
-  #   names(all_citations)[which(names(all_citations) ==  "tag")] <- "label"
-  #   
-  #   cols_needed <- c("title","author","year","pages", "volume", "number", "journal", "doi", "isbn", "abstract", "record_id", "label", "source")
-  #   all_citations[cols_needed[!(cols_needed %in% colnames(all_citations))]] = NA
-  #   
-  #   all_citations <- all_citations %>%
-  #     mutate(pages = ifelse(is.na(pages), paste0(start_page, "-", end_page), paste(pages))) %>%
-  #     mutate(pages = ifelse(pages == "NA-NA", NA, paste(pages)))
-  #   
-  #   result <- dedup_citations(all_citations, merge_citations = TRUE)
-  #   
-  #   unique_citations <- result$unique
-  #   
-  # })
-  # 
-  # 
-  # # output for comparison table across souces
-  # output$comparison_table <- renderDataTable({
-  #   
-  #   source_comparison <- function(unique_data){
-  #     
-  #     db_comparison <- unique_data %>%
-  #       select(duplicate_id, label, source, record_id) %>%
-  #       separate_rows(c(source, label), convert = TRUE, sep=", ") %>%
-  #       unique() %>%
-  #       pivot_wider(id_col = duplicate_id, names_from=c(source), values_from=c(record_id),
-  #                   values_fn =  function(x) paste("x"),
-  #                   values_fill = "")
-  #     
-  #     unique_data$author <- gsub(",.*", "", unique_data$author)
-  #     
-  #     pub_id <- unique_data %>%
-  #       select(author, year, duplicate_id) %>%
-  #       mutate(pub_id = paste0(author, "-", year)) %>%
-  #       select(-author, -year) %>%
-  #       select(pub_id, everything())
-  #     
-  #     db_comparison <- left_join(db_comparison, pub_id, by="duplicate_id")
-  #   }
-  #   
-  #   
-  #   comparison <- source_comparison(unique_citations())
-  #   
-  #   DT::datatable(comparison,
-  #                 options = list(paging = FALSE, 
-  #                                searching = FALSE),
-  #                 rownames = FALSE)
-  # })
-  # 
-  # # output results table from deduplication pre/post numbers per source and overall
-  # output$dedup_results_table <- function() {
-  #   
-  #   
-  #   req(unique_citations())
-  #   
-  #   rem_dup_word <- function(x){
-  #     x <- tolower(x)
-  #     paste(unique(trimws(unlist(strsplit(x,split=", ",fixed=F,perl=T)))),collapse = 
-  #             ", ")
-  #   }
-  #   
-  #   # pre-dedup table
-  #   pre_dedup <- rv$upload_df %>%
-  #     group_by(cs_source) %>%
-  #     count()  %>%
-  #     pivot_wider(names_from=c(cs_source), values_from=n) 
-  #   
-  #   #post-dedup table
-  #   post_dedup <- unique_citations() %>%
-  #     select(duplicate_id, label, source, record_id) %>%
-  #     separate_rows(c(source, label), convert = TRUE, sep=", ") %>%
-  #     unique() %>%
-  #     pivot_wider(id_col = duplicate_id, names_from=c(source), values_from=c(record_id),
-  #                 values_fn = list(record_id = length)) %>%
-  #     select_if(is.numeric) %>%
-  #     summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE))) 
-  #   
-  #   combined_dedup_table <- rbind(pre_dedup, post_dedup)
-  #   combined_dedup_table <- cbind(combined_dedup_table, 
-  #                                 Total = rowSums(combined_dedup_table)) 
-  #   
-  #   combined_dedup_table$Total[2] <- paste0(nrow(unique_citations()), " merged unique citations")
-  #   
-  #   combined_dedup_table %>%
-  #     knitr::kable("html",  caption = "De-duplication results") %>%
-  #     kable_styling("striped", full_width = T)  %>%
-  #     pack_rows(
-  #       index = c("Before deduplication" = 1, "After deduplication" = 1))
-  #   
-  #   
-  # }
-  # 
-  # 
-  # #### end section #### year,
-  # 
-  # 
-  # 
-  # 
-  # #### Visualise section ####
-  # #render check box ui based on uploaded sources and tags
-  # output$checkbox1 <- renderUI({
-  #   checkboxGroupInput('source_check', 'Sources', unique(rv$df$source))
-  # })
-  # output$checkbox2 <- renderUI({
-  #   checkboxGroupInput('tag_check', 'Tags', unique(rv$df$tag))
-  # })
-  # 
+
+  # Add data for testing 
+  dedup_results <- readRDS("www/dedup.RDS") #TODO remove when the upload and dedup is finished
   
   #### end section ####
   
+  #### Visualise tab ####
+  output$plotgraph1<-renderPlotly({
+    unique_citations <- dedup_results$unique
+  n_unique <- count_unique(unique_citations)
+  # for each unique citation, which sources are present
+  source_comparison <- compare_sources(unique_citations, comp_type = "sources")
+  label_comparison <- compare_sources(unique_citations, comp_type = "labels")
+  n_unique$cite_label <- factor(n_unique$cite_label, levels=c('search','screen','final'))
+  plot_source_overlap_heatmap(source_comparison)
+  })
+
+output$plotgraph2<-renderPlot({
+  plot_source_overlap_upset(source_comparison, decreasing = c(TRUE, TRUE))
+})
+
+
+output$plotgraph2 <- renderPlot({
+  myplot <- plot_source_overlap_upset(source_comparison, decreasing = c(TRUE, TRUE))
+  myplot
+})
+
+plotInput = function() {
   
-  #### Export section ####
+  ##################################################
+  ##################################################
+  #This doesn't work - upset plots not supported
+  unique_citations <- dedup_results$unique
+  n_unique <- count_unique(unique_citations)
+    source_comparison <- compare_sources(unique_citations, comp_type = "sources")
+  plot_source_overlap_upset(source_comparison, decreasing = c(TRUE, TRUE))
+  ##################################################
+  ##################################################
   
-  #### end section ####
-  
+}
+
+output$downloadPlot <- downloadHandler(
+  filename = function() { paste("test", '.svg', sep='') },
+  content = function(file) {
+    ggsave(file, plot = plotInput(), device = "svg")
+  }
+)
+
+
 }
 
 # Create Shiny app ----
