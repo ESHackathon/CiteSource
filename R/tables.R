583e21c2f900e1cefc9f17072aac0b3c4d78c0af
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-12-01 10:28:09 +0100

    Add record-level table

--- R/tables.R
+++ R/tables.R
@@ -4,16 +4,109 @@
 #'
 #' @param citations A deduplicated tibble as returned by `dedup_citations()`.
 #' @param include Which metadata should be included in the table? Defaults to 'sources', can be replaced or expanded with 'labels' and/or 'strings'
-#' @return A tibble that can be turned into a pretty table with (e.g.) the `gt`-package
+#' @param indicator_presence How should it be indicated that a value is present in a source/label/string? Defaults to TRUE in tibbles and a tickmark in DT tables
+#' @param indicator_absence How should it be indicated that a value is *not* present in a source/label/string? Defaults to FALSE in tibbles and a cross in DT tables
+#' @param return Either a `tibble` that can be exported, e.g. as a csv, or a DataTable (`DT`) that allows for interactive exploration
 #' @export
 
-record_level_table <- function(citations, include = "sources") {
-  warning("Not yet implemented.")
-  sources <<- compare_sources(citations, comp_type = include)
-  citations <<- citations
+record_level_table <- function(citations, include = "sources", return = c("tibble", "DT"), indicator_presence = NULL, indicator_absence = NULL) {
+  if (is.null(indicator_absence)) {
+    indicator_absence <- switch(return[1],
+      tibble = FALSE,
+      DT = "&#10004;"
+    )
+  }
+  if (is.null(indicator_presence)) {
+    indicator_presence <- switch(return[1],
+      tibble = TRUE,
+      DT = "&#x2717;"
+    )
+  }
+  sources <- compare_sources(citations, comp_type = include)
+  citations <- citations %>%
+    dplyr::mutate(
+      citation = generate_apa_citation(author, year),
+      reference = generate_apa_reference(author, year, title, source, volume, issue, doi, url),
+      html_reference = generate_apa_reference(author, year, title, source, volume, issue, doi, url, return_html = TRUE)
+    ) %>%
+    # Arrange by first author to avoid issue with initials
+    dplyr::arrange(stringr::str_extract(author, "^.*?,"), citation) %>%
+    dplyr::select(duplicate_id, citation, reference, html_reference) %>%
+    dplyr::left_join(sources, by = "duplicate_id")
+
+  if (!(is.logical(indicator_presence) & is.logical(indicator_absence))) {
+    indicator_presence <- as.character(indicator_presence)
+    indicator_absence <- as.character(indicator_absence)
+    citations <- citations %>% dplyr::mutate(dplyr::across(-c(1:3), as.character))
+    citations[-c(1:3)][citations[-c(1:3)] == "TRUE"] <- indicator_presence
+    citations[-c(1:3)][citations[-c(1:3)] == "FALSE"] <- indicator_absence
+  }
+  if (return[1] == "DT") {
+    if (!rlang::is_installed("DT")) {
+      warning('DT can only be returned when the DT package is installed. Please run install.packages("DT")')
+      return(citations)
+    } else {
+      headings <- purrr::map(include, function(type) {
+        type <- stringr::str_sub(type, 1, -2)
+        values <- names(citations) %>%
+          stringr::str_subset(glue::glue("^{type}__")) %>%
+          stringr::str_remove(glue::glue("^{type}__"))
+        list(type = type %>% stringr::str_to_title(), values = values)
+      }) %>% purrr::transpose()
+
+
+      sketch <- htmltools::withTags(table(
+        class = "display",
+        thead(
+          tr(
+            th(rowspan = 2, colspan = 2, htmltools::HTML("&nbsp;")),
+            th(rowspan = 2, colspan = 2, "Citation"),
+            purrr::map2(headings$type, lengths(headings$values), ~ th(colspan = .y, .x))
+          ),
+          tr(
+            purrr::map(unlist(headings$values), ~ th(.x))
+          )
+        ),
+        tfoot(
+          td(colspan = 4 + length(unlist(headings$values)), htmltools::HTML("Click on the &oplus; to view the full reference"))
+        )
+      ))
+
+      citations %>%
+        dplyr::select(-"duplicate_id", -"reference") %>%
+        cbind(" " = "&oplus;", .) %>%
+        DT::datatable(
+          escape = FALSE,
+          options = list(
+            columnDefs = list(
+              list(visible = FALSE, targets = c(0, 3)),
+              list(orderable = FALSE, className = "details-control", targets = 1)
+            )
+          ), container = sketch,
+          callback = DT::JS("
+              table.column(1).nodes().to$().css({cursor: 'pointer'});
+              var format = function(d) {
+                return '<div style=\"background-color:#eee; padding: .5em;\">' +
+                        d[3];
+              };
+              table.on('click', 'td.details-control', function() {
+                var td = $(this), row = table.row(td.closest('tr'));
+                if (row.child.isShown()) {
+                  row.child.hide();
+                  td.html('&oplus;');
+                } else {
+                  row.child(format(row.data())).show();
+                  td.html('&CircleMinus;');
+                }
+              });")
+        )
+    }
+  } else {
+    citations %>% dplyr::select(-"html_reference")
+  }
 }
 
-#' Summary table
+#' Contribution summary table
 #'
 #' Create a summary table to show the contribution of each source and the overall performance of the search. For this to work, 
 #' labels need to be used that contrast a "search" stage with one or more later stages.
@@ -94,3 +187,211 @@ citation_summary_table <- function(citations, comparison_type = "sources", searc
     gt::fmt_number(3:5, decimals = 0)
   
 }
+
+#' Generate author-year citations for record table
+#'
+#' Creates the author-year citations (e.g., `Miller (2020)`) for the record-level table. This requires a lot of disambiguation
+#' when similar author-year combinations appear. This function largely follows APA style, yet abbreviates citations where more than 
+#' five authors would have to be listed.
+#'
+#' @param authors Character vector of authors. Needs to follow Last, First and Last, First etc format (as returned by dedup)
+#' @param year Vector of publication years
+#' @return A character vector of citations
+#' @noRd
+
+
+generate_apa_citation <- function(authors, year) {
+  
+  id <- seq_along(authors)
+  # Extract last names and initials
+  processed_names <- tibble::tibble(id = id, authors = authors, year = year) %>% 
+    mutate(last_names = stringr::str_replace_all(authors, "(,.*? and)", "__") %>% stringr::str_remove(",.*$") %>% stringr::str_split("__ "),
+           initials = stringr::str_replace_all(authors, "( and.*?, )", "__") %>% stringr::str_remove("^.*?,") %>% 
+             stringr::str_split("__") %>% purrr::map(~.x %>% stringr::str_remove_all("\\.") %>% 
+                                                       stringr::str_trim() %>%
+                                                       stringr::str_split(pattern = " ") %>%
+                                                       purrr::map(stringr::str_trunc, 1, ellipsis = "") %>%
+                                                       purrr::map(stringr::str_c, collapse=". ") %>%
+                                                       purrr::flatten_chr() %>% 
+                                                       paste0(".")))
+           
+ # If last name does not uniquely describe authors, first author should be disambiguated in APA style
+ # Here implemented by comparing initials -
+ # False positives where some sources contain two names (or initials) while others include 1
+ # False negative where same initial refers to different names
+ # Appears to be best balance for now - further options and instructions could be provided
+
+  last_name <- processed_names %>% dplyr::pull(last_names) %>% unlist()
+  initialed_name_list <- processed_names %>% dplyr::select(last_names, initials) %>% as.list() %>% 
+    purrr::transpose() %>% purrr::map(~paste(.x[[2]], .x[[1]])) 
+
+  initialed_name <- initialed_name_list %>% unlist()
+  
+  authors <- tibble::tibble(last_name, initialed_name)
+  duplicated_last_names <- last_name[duplicated(last_name)]
+  # Identify which last names appear with different initials
+  to_disambiguate <- authors %>% dplyr::filter(.data$last_name %in% duplicated_last_names) %>% 
+    dplyr::group_by(.data$last_name) %>% 
+    dplyr::summarise(disambiguate = dplyr::n_distinct(.data$initialed_name) > 1) %>% 
+    dplyr::filter(disambiguate == TRUE) %>% dplyr::pull(.data$last_name)
+    
+  # Replace those last names with initials
+  for (i in seq_along(processed_names$last_names)) {
+      if (processed_names$last_names[[i]][[1]] %in% to_disambiguate) {
+        processed_names$last_names[[i]][[1]] <- initialed_name_list[[i]][[1]]
+      }
+    }
+    
+  # Create simple citations
+  citations <- processed_names %>% dplyr::rowwise() %>% 
+    dplyr::mutate(N_authors = length(.data$last_names),
+                  year = as.character(.data$year),
+                  citation = dplyr::case_when(
+                    .data$N_authors == 1 ~ glue::glue("{last_names[1]} ({year})"),
+                    .data$N_authors == 2 ~ glue::glue("{last_names[1]} & {last_names[2]} ({year})"),
+                    .data$N_authors > 2 ~ glue::glue("{last_names[1]} et al. ({year})")
+                    )) %>% 
+    dplyr::ungroup()
+  
+  # Disambiguate
+  citations_unambiguous <- citations %>% 
+    dplyr::filter(!(duplicated(citation) | (duplicated(citation, fromLast = TRUE))))
+  
+  citations_ambiguous <- citations %>% 
+    dplyr::filter((duplicated(citation) | (duplicated(citation, fromLast = TRUE))))
+
+  if (nrow(citations_ambiguous) > 0) {
+  
+  citations_ambiguous <- purrr::map_dfr(unique(citations_ambiguous$citation), function (current_citation) {
+    group <- citations_ambiguous %>% dplyr::filter(.data$citation == current_citation) %>% 
+      dplyr::mutate(author_names = purrr::map(.data$last_names, stringr::str_c())) %>% 
+      dplyr::arrange(dplyr::desc(.data$author_names))
+    
+    # Case 1: multiple publications by same author(s) in same year - add letters
+    if (dplyr::n_distinct(group$author_names) == 1) {
+      group %>% dplyr::mutate(year = paste0(year, letters[1:dplyr::n()])) %>% dplyr::rowwise() %>% 
+        dplyr::mutate(N_authors = length(last_names), 
+                      citation = dplyr::case_when(
+                        .data$N_authors == 1 ~ glue::glue("{last_names[1]} ({year})"),
+                        .data$N_authors == 2 ~ glue::glue("{last_names[1]} & {last_names[2]} ({year})"),
+                        .data$N_authors > 2 ~ glue::glue("{last_names[1]} et al. ({year})")
+                      )) %>% 
+        dplyr::ungroup()
+    } else {
+    # Case 2: distinct authors
+      #Find maximum number of common authors
+      pairs <- combn(group$last_names, 2)
+      common <- numeric()
+      for (i in seq_len(ncol(pairs))) {
+        first <- pairs[[1,i]]
+        second <- pairs[[2,i]]
+        len <- min(length(first), length(second))
+        comparison <- suppressWarnings(first[1:len] == second[1:len])
+        #Common authors: either those before first divergence, or all 
+        common <- c(common, dplyr::coalesce(which(comparison == FALSE)[1]-1, length(comparison)))
+      }
+      common <- max(common)
+
+        group %>% dplyr::rowwise() %>% 
+          dplyr::mutate(citation = dplyr::case_when(
+          (common < 5 | .data$N_authors < 5) & N_authors < common + 3 ~ glue::glue("{glue::glue_collapse(last_names, ', ', last = ' & ')} ({year})"),
+          (common < 5 | .data$N_authors < 5) ~ glue::glue("{glue::glue_collapse(last_names[1:(common+1)], ', ')} et al. ({year})"),
+          common >= 5 & .data$N_authors < common + 2  ~ glue::glue("{last_names[1]} ... {last_names[common]}  & {last_names[common+1]} ({year})"),
+          common >= 5 & .data$N_authors < common + 3  ~ glue::glue("{last_names[1]} ... {last_names[common+1]}  & {last_names[common+2]} ({year})"),
+          common >= 5  ~ glue::glue("{last_names[1]} ... {last_names[common+1]} et al. ({year})")
+        )) %>% dplyr::ungroup()
+    }  
+  })
+  
+  citations_still_ambiguous <- citations_ambiguous  %>% 
+    dplyr::filter((duplicated(citation) | (duplicated(citation, fromLast = TRUE))))
+  
+  citations_unambiguous <- dplyr::bind_rows(citations_unambiguous, citations_ambiguous  %>% 
+                                       dplyr::filter(!(duplicated(citation) | (duplicated(citation, fromLast = TRUE)))))
+  
+  # If some of Case 2 were in fact Case 1s (e.g., more than 2 authors with same names), they need to be further disambiguated
+  citations_still_ambiguous <- citations_still_ambiguous %>% dplyr::group_by(citation) %>% 
+    dplyr::mutate(letter = letters[1:dplyr::n()]) %>% dplyr::rowwise() %>% 
+    dplyr::mutate(citation = stringr::str_replace(citation, "([:digit:])\\)", glue::glue("\\1{letter})"))) %>% 
+    dplyr::ungroup()
+  
+  citations_unambiguous <- dplyr::bind_rows(citations_unambiguous, citations_still_ambiguous)
+  
+  }
+  citations_unambiguous %>% 
+    dplyr::left_join(tibble::tibble(id), ., by = "id") %>% dplyr::pull(citation)
+  
+}
+
+
+#' Generate apa-style reference for record table
+#'
+#' Creates the full references based on the deduplicated data - for instance to show as additional detail in the 
+#' record-level table. Citations are generally formatted like those for journal articles - if we want more formats, 
+#' we should rely on a specialist package, but this seems ok for now. Page numbers are not included for now, as they 
+#' do not seem to be returned clearly in deduplication?
+#'
+#' @param authors Character vector of authors. Needs to follow Last, First and Last, First etc format (as returned by dedup)
+#' @param year Vector of publication years
+#' @param title Vector of titles
+#' @param source Journal or other publication
+#' @param volume Volume - can be blank
+#' @param issue Issue - can be blank.
+#' @param doi - will be converted to link format
+#' @param weblink - will be coalesced with DOI; DOI will be given preference.
+#' @param return_html Should HTML-formatted reference (with link and journal in italics) be returned
+#' @param format_journal_case Should source be formatted to title case (otherwise, journal titles are often in ALL CAPS or similar)
+#' @return A character vector of references
+#' @noRd
+
+
+generate_apa_reference <- function(authors, year, title, source, volume, issue, doi, weblink, return_html = FALSE, format_journal_case = TRUE) {
+  id <- seq_along(authors)
+  
+  # Extract last names and initials
+  citations <- tibble::tibble(id, authors, year, title, source, volume, issue, doi, weblink) %>% 
+    dplyr::mutate(dplyr::across(.fns = ~dplyr::na_if(.x, ""))) %>% 
+    mutate(last_names = stringr::str_replace_all(authors, "(,.*? and)", "__") %>% stringr::str_remove(",.*$") %>% stringr::str_split("__ "),
+           initials = stringr::str_replace_all(authors, "( and.*?, )", "__") %>% stringr::str_remove("^.*?,") %>% 
+             stringr::str_split("__") %>% purrr::map(~.x %>% stringr::str_remove_all("\\.") %>% 
+                                                       stringr::str_trim() %>%
+                                                       stringr::str_split(pattern = " ") %>%
+                                                       purrr::map(stringr::str_trunc, 1, ellipsis = "") %>%
+                                                       purrr::map(stringr::str_c, collapse=". ") %>%
+                                                       purrr::flatten_chr() %>% 
+                                                       paste0(".")))
+  # Merge initials to names
+  citations$initialed_names <-   citations %>% dplyr::select(last_names, initials) %>% as.list() %>% 
+    purrr::transpose() %>% purrr::map(~paste(.x[[1]], .x[[2]], sep = ", "))
+
+  if (format_journal_case) {
+    citations <- citations %>% dplyr::mutate(source = stringr::str_to_title(source))
+  }
+  
+  # Helper function to deal with missing values 
+  nNA <- function (x, ..., alt = "", pre = "") {
+    ifelse(is.na(x), alt, paste0(pre, x, ...))
+  }
+  
+  # Compose references
+  
+  citations <- citations %>% dplyr::mutate(doi = dplyr::if_else(stringr::str_detect(doi, "http"), doi, paste0("https://doi.org/", doi)),
+                              link = coalesce(doi, weblink))
+  
+  if (return_html) {
+  citations %>%
+    dplyr::rowwise() %>% dplyr::mutate(
+                              reference = glue::glue("
+                              {glue::glue_collapse(initialed_names, ', ', last = ' & ')} ({year}). {title}. {nNA(source, pre = '<i>', '</i>')}{nNA(volume, pre = '<i>, ', '</i>')}{nNA(issue, pre = '(', ')')}. {nNA(link, pre = '<a href = \"', '\">')}{nNA(link, '</a>')}
+                                                     ")) %>% dplyr::pull(reference)
+  } else {
+    citations %>%
+      dplyr::rowwise() %>% dplyr::mutate(
+        reference = glue::glue("
+                              {glue::glue_collapse(initialed_names, ', ', last = ' & ')} ({year}). {title}. {nNA(source)}{nNA(volume, pre = ', ')}{nNA(issue, pre = '(', ')')}. {nNA(link)}
+                                                     ")) %>% dplyr::pull(reference)
+  }
+    
+}
+
+
